38599f93da6694074083a24e8300b288c8b93002
Mario E Carrizosa Villarreal <mytcarrizosaland@gmail.com>
2023-08-07 11:43:56 -0600

    Challenge Tecnico

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [rojo, setRojo] = useState(true);
+const [amarillo, setAmarillo]=useState(false);
+const [verde, setVerde] = useState(false)
+
+const cambiarLuz = () => {
+
+  if(rojo){
+    setRojo(!rojo)
+    setAmarillo(!amarillo)
+  }
+  if(amarillo){
+    setAmarillo(!amarillo)
+    setVerde (!verde)
+  }
+  if(verde){
+    setVerde(!verde)
+    setRojo(!rojo)
+  }
+
+  console.log('rojo', rojo);
+  console.log('amar', amarillo);
+  console.log('verde', verde);
+}
+
+  // useEffect(()=> {
+  //   cambiarLuz()
+  // }, [verde, amarillo, rojo])
+
+  setTimeout(() => {
+    cambiarLuz()
+  }, 3000);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='semaforo'>
+        {!rojo? <span className='off'></span>: <span className='rojo'></span> }
+        {!amarillo? <span className='off'></span>: <span className='amarillo'></span> }
+        {!verde? <span className='off'></span>: <span className='verde'></span> }
+      </div>
     </div>
   );
 }
